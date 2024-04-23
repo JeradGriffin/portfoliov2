ba4bbd4bf2a18588193f3da1d0701c660d8dc04e
Jerad Griffin <9543616+JeradGriffin@users.noreply.github.com>
2024-04-23 13:44:15 -0400

    set up

---
+++ eleventy.config.js
@@ -0,0 +1,33 @@
+import pluginWebc from "@11ty/eleventy-plugin-webc";
+
+export default (eleventyConfig) => {
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: "src/_components/**/*.webc",
+  });
+
+  eleventyConfig.addJavaScriptFunction("renderTitle", (title) => {
+    return [...new Set([title, "Jerad Griffin"])].filter((part) => !!part).join(" | ");
+  });
+
+  eleventyConfig.addJavaScriptFunction("renderSocialImage", (socialImage) => {
+    if (socialImage) {
+      if (!(socialImage.startsWith("http://") || socialImage.startsWith("https://"))) {
+        socialImage = `https://www.griffindesdev.com${socialImage}`;
+      }
+      return socialImage;
+    }
+    return `https://www.griffindesdev.com/assets/share-graphic.png`;
+  });
+
+  eleventyConfig.addGlobalData(
+    "copyright",
+    () =>
+      `&copy; ${[...new Set([2020, new Date().getFullYear()])].join("-")} Jerad Griffin.`
+  );
+
+  return {
+    dir: {
+      input: "src",
+    },
+  };
+};
